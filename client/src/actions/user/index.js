85e5519f7df13c68db2f950dcb7f8c77cb92a7d1
Marcus Young <monkeyyoung3@gmail.com>
2020-06-10 21:38:58 -0700

    minor changes

--- client/src/actions/user/index.js
+++ client/src/actions/user/index.js
@@ -4,7 +4,7 @@ import { GET_USER_INFO, GET_USER_INFO_ERROR} from '../types';
 
 export const getUserInfo = () => async dispatch  => {
     try {
-        const { data } = axios.get('/api/user')
+        const { data } = axios.get(`/api/user?user=${}`)
         dispatch({ type: GET_USER_INFO, payload: data })
     } catch (e) {
         dispatch({ type: GET_USER_INFO_ERROR, payload: e })
