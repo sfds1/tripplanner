dd78a643a6979a22f9b37d5e1b7ebeded55458d3
Marcus Young <monkeyyoung3@gmail.com>
2020-06-10 21:56:48 -0700

    added get user data controller and router by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,4 +9,12 @@ module.exports = {
       return res.status(403).json({ e });
     }
   },
+  getUserData: async (req, res) => {
+    try {
+      const user = await User.findById(req.user._id);
+      return res.status(200).json(user);
+    } catch (e) {
+      return res.status(403).json({ e });
+    }
+  },
 };
