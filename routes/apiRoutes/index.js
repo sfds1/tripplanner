bf3f4eebda4efc608af95902fad986bde78558ca
SK <stephanie@sfdigitalstudio.com>
2020-06-10 19:10:15 -0700

    added user routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const authRoutes = require('./authRoutes');
+const userRoutes = require('./userRoutes');
 const tripRoutes = require('./tripRoutes');
 const categoryRoutes = require('./categoryRoutes');
 const activityRoutes = require('./activityRoutes');
@@ -7,6 +8,7 @@ const commentRoutes = require('./commentRoutes');
 
 
 router.use('/auth', authRoutes);
+router.use('/user', userRoutes);
 router.use('/trip', tripRoutes);
 router.use('/category', categoryRoutes);
 router.use('/activity', activityRoutes);
