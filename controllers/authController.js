415c77bacfa7de95eba745cf375d1882ca3df5ab
Marcus Young <monkeyyoung3@gmail.com>
2020-06-10 18:50:59 -0700

    added userController file

--- controllers/authController.js
+++ controllers/authController.js
@@ -54,12 +54,4 @@ module.exports = {
     }
   },
   signIn: (req, res) => res.json({ token: tokenForUser(req.user) }),
-  getAllUserEmails: async (req, res) => {
-    try {
-      const userEmail = await User.findOne({ email: req.query.email }, 'email');
-      return res.status(200).json(userEmail);
-    } catch (e) {
-      return res.status(403).json({ e });
-    }
-  },
 };
