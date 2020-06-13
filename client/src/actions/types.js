ec13fcbc680509ddfedf70fd26525c18cfde230a
SK <stephanie@sfdigitalstudio.com>
2020-06-13 10:51:58 -0700

    updated action types

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,11 +1,77 @@
+//  User authentication
+export const AUTH_USER = 'AUTH_USER';
+export const AUTH_USER_ERROR = 'AUTH_USER_ERROR';
+
+// Get or update user info
 export const GET_USER_INFO = 'GET_USER_INFO'
 export const GET_USER_INFO_ERROR = 'GET_USER_INFO_ERROR';
 
+export const UPDATE_USER_INFO = 'UPDATE_USER_INFO'
+export const UPDATE_USER_INFO_ERROR = 'UPDATE_USER_INFO_ERROR'
+
+// Get, add or delete friend to a trip
 export const GET_FRIEND_BY_EMAIL = 'GET_FRIEND_BY_EMAIL';
 export const GET_FRIEND_BY_EMAIL_ERROR = 'GET_FRIEND_BY_EMAIL_ERROR';
 
+export const ADD_FRIEND_BY_EMAIL = 'ADD_FRIEND_BY_EMAIL';
+export const ADD_FRIEND_BY_EMAIL_ERROR = 'ADD_FRIEND_BY_EMAIL_ERROR';
+
+export const DELETE_FRIEND_BY_EMAIL = 'DELETE_FRIEND_BY_EMAIL';
+export const DELETE_FRIEND_BY_EMAIL_ERROR = 'DELETE_FRIEND_BY_EMAIL_ERROR';
+
+// Get, add, update or delete trips
 export const GET_USER_TRIPS = 'GET_USER_TRIPS';
 export const GET_USER_TRIPS_ERROR = 'GET_USER_TRIPS_ERROR';
+
+export const ADD_USER_TRIPS = 'ADD_USER_TRIPS';
+export const ADD_USER_TRIPS_ERROR = 'ADD_USER_TRIPS_ERROR';
+
+export const UPDATE_USER_TRIPS = 'UPDATE_USER_TRIPS';
+export const UPDATE_USER_TRIPS_ERROR = 'UPDATE_USER_TRIPS_ERROR';
+
+export const DELETE_USER_TRIPS = 'DELETE_USER_TRIPS';
+export const DELETE_USER_TRIPS_ERROR = 'DELETE_USER_TRIPS_ERROR';
+
+// Get, add, update or delete category
+export const GET_CATEGORY = 'GET_CATEGORY';
+export const GET_CATEGORY_ERROR = 'GET_CATEGORY_ERROR';
+
+export const ADD_CATEGORY = 'ADD_CATEGORY';
+export const ADD_CATEGORY_ERROR = 'ADD_CATEGORY_ERROR';
+
+export const UPDATE_CATEGORY = 'UPDATE_CATEGORY';
+export const UPDATE_CATEGORY_ERROR = 'UPDATE_CATEGORY_ERROR';
+
+export const DELETE_CATEGORY = 'DELETE_CATEGORY';
+export const DELETE_CATEGORY_ERROR = 'DELETE_CATEGORY_ERROR';
+
+// Get, add, update or delete activity
+export const GET_ACTIVITY = 'GET_ACTIVITY';
+export const GET_ACTIVITY_ERROR = 'GET_ACTIVITY_ERROR';
+
+export const ADD_ACTIVITY = 'ADD_ACTIVITY';
+export const ADD_ACTIVITY_ERROR = 'ADD_ACTIVITY_ERROR';
+
+export const UPDATE_ACTIVITY = 'UPDATE_ACTIVITY';
+export const UPDATE_ACTIVITY_ERROR = 'UPDATE_ACTIVITY_ERROR';
+
+export const DELETE_ACTIVITY = 'DELETE_ACTIVITY';
+export const DELETE_ACTIVITY_ERROR = 'DELETE_ACTIVITY_ERROR';
+
+// Get, add, update or delete comment
+export const GET_COMMENT = 'GET_COMMENT';
+export const GET_COMMENT_ERROR = 'GET_COMMENT_ERROR';
+
+export const ADD_COMMENT = 'ADD_COMMENT';
+export const ADD_COMMENT_ERROR = 'ADD_COMMENT_ERROR';
+
+export const UPDATE_COMMENT = 'UPDATE_COMMENT';
+export const UPDATE_COMMENT_ERROR = 'UPDATE_COMMENT_ERROR';
+
+export const DELETE_COMMENT = 'DELETE_COMMENT';
+export const DELETE_COMMENT_ERROR = 'DELETE_COMMENT_ERROR';
+
+
 
 export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
@@ -32,6 +98,4 @@ export const UPDATE_TODO_BY_ID_ERROR = 'UPDATE_TODO_BY_ID_ERROR';
 export const DELETE_TODO_BY_ID_ERROR = 'DELETE_TODO_BY_ID_ERROR';
 
 
-//  User authentication
-export const AUTH_USER = 'AUTH_USER';
-export const AUTH_USER_ERROR = 'AUTH_USER_ERROR';
+
